1a463c15212d3781941d0c74be8fa525f0833e54
JuliCarracedo <julicarra@hotmail.com>
2021-09-16 15:55:26 -0300

    Add extra logic to Calculator

--- src/App.js
+++ src/App.js
@@ -3,10 +3,31 @@ import { Component } from 'react';
 import Calculator from './components/Calculator';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      calculatorObj: {
+        total: null,
+        next: null,
+        operation: null,
+      },
+    };
+    this.updateState = this.updateState.bind(this);
+  }
+
+  updateState(obj) {
+    this.setState(
+      {
+        calculatorObj: obj,
+      },
+    );
+  }
+
   render() {
+    const { calculatorObj } = this.state;
     return (
       <body>
-        <Calculator />
+        <Calculator updateState={this.updateState} calculatorObj={calculatorObj} />
       </body>
     );
   }
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,44 +3,105 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 
 import { Component } from 'react';
+import PropTypes from 'prop-types';
 import calculate from '../logic/calculate';
 
 class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.calculate = this.calculate.bind(this);
+    this.displayResult = this.displayResult.bind(this);
+  }
+
+  calculate(obj, button) {
+    const { updateState } = this.props;
+    let newObj;
+    try {
+      newObj = calculate(obj, button);
+    } catch {
+      newObj = {
+        total: "Can't perform module operation by 0",
+      };
+    }
+    this.calculatorObj = newObj;
+    updateState(newObj);
+  }
+
+  displayResult() {
+    const { calculatorObj } = this.props;
+    const { total } = calculatorObj;
+    const { next } = calculatorObj;
+    const { operation } = calculatorObj;
+    let result = null;
+
+    if (total === null && next === null) {
+      result = 0;
+    } else if (next !== null && total === null) {
+      result = next;
+    } else if (next !== null && total !== null) {
+      if (operation !== null && operation !== undefined) {
+        result = `${total} ${operation} ${next}`;
+      } else {
+        result = total;
+      }
+    } else if (next === null && total !== null) {
+      if (operation !== null && operation !== undefined) {
+        result = `${total} ${operation}`;
+      } else {
+        result = total;
+      }
+    }
+
+    return result;
+  }
+
   render() {
+    const { calculatorObj } = this.props;
     return (
       <div className="table">
-        <div className="row field"><p id="field-text">Welcome</p></div>
-        <div className="row">
-          <div className="button gray" onClick={calculate(obj, 'AC')}>AC</div>
-          <div className="button gray" onClick={calculate(obj, 'AC')}>+/-</div>
-          <div className="button gray" onClick={calculate(obj, 'AC')}>%</div>
-          <div className="button orange" onClick={calculate(obj, 'AC')}>/</div>
+        <div className="row field">
+          <p id="field-text">
+            {' '}
+            { this.displayResult() }
+          </p>
         </div>
         <div className="row">
-          <div className="button gray">7</div>
-          <div className="button gray">8</div>
-          <div className="button gray">9</div>
-          <div className="button orange">X</div>
+          <div className="button gray" onClick={() => { this.calculate(calculatorObj, 'AC'); }}>AC</div>
+          <div className="button gray" onClick={() => { this.calculate(calculatorObj, '+/-'); }}>+/-</div>
+          <div className="button gray" onClick={() => { this.calculate(calculatorObj, '%'); }}>%</div>
+          <div className="button orange" onClick={() => { this.calculate(calculatorObj, '÷'); }}>÷</div>
         </div>
         <div className="row">
-          <div className="button gray">4</div>
-          <div className="button gray">5</div>
-          <div className="button gray">6</div>
-          <div className="button orange">-</div>
+          <div className="button gray" onClick={() => { this.calculate(calculatorObj, '7'); }}>7</div>
+          <div className="button gray" onClick={() => { this.calculate(calculatorObj, '8'); }}>8</div>
+          <div className="button gray" onClick={() => { this.calculate(calculatorObj, '9'); }}>9</div>
+          <div className="button orange" onClick={() => { this.calculate(calculatorObj, 'x'); }}>X</div>
         </div>
         <div className="row">
-          <div className="button gray">1</div>
-          <div className="button gray">2</div>
-          <div className="button gray">3</div>
-          <div className="button orange">+</div>
+          <div className="button gray" onClick={() => { this.calculate(calculatorObj, '4'); }}>4</div>
+          <div className="button gray" onClick={() => { this.calculate(calculatorObj, '5'); }}>5</div>
+          <div className="button gray" onClick={() => { this.calculate(calculatorObj, '6'); }}>6</div>
+          <div className="button orange" onClick={() => { this.calculate(calculatorObj, '-'); }}>-</div>
         </div>
         <div className="row">
-          <div className="big-button gray">0</div>
-          <div className="button gray">.</div>
-          <div className="button orange">=</div>
+          <div className="button gray" onClick={() => { this.calculate(calculatorObj, '1'); }}>1</div>
+          <div className="button gray" onClick={() => { this.calculate(calculatorObj, '2'); }}>2</div>
+          <div className="button gray" onClick={() => { this.calculate(calculatorObj, '3'); }}>3</div>
+          <div className="button orange" onClick={() => { this.calculate(calculatorObj, '+'); }}>+</div>
+        </div>
+        <div className="row">
+          <div className="big-button gray" onClick={() => { this.calculate(calculatorObj, '0'); }}>0</div>
+          <div className="button gray" onClick={() => { this.calculate(calculatorObj, '.'); }}>.</div>
+          <div className="button orange" onClick={() => { this.calculate(calculatorObj, '='); }}>=</div>
         </div>
       </div>
     );
   }
 }
+
+Calculator.propTypes = {
+  updateState: PropTypes.func.isRequired,
+  calculatorObj: PropTypes.instanceOf(Object).isRequired,
+};
+
 export default Calculator;
