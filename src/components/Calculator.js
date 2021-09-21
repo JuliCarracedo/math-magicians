ad35d79d41262e098aa5e19badd31daf3db85de8
JuliCarracedo <julicarra@hotmail.com>
2021-09-21 15:53:47 -0300

    Add routing

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,7 +2,12 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 
 import PropTypes from 'prop-types';
+import { Switch, Route } from 'react-router';
 import calculate from '../logic/calculate';
+import Quote from '../pages/Quote';
+import NotMatch from '../pages/NoMatch';
+import Navbar from './Navbar';
+import Home from '../pages/Home';
 
 const Calculator = (props) => {
   let { calculatorObj } = props;
@@ -45,42 +50,63 @@ const Calculator = (props) => {
   };
 
   return (
-    <div className="table">
-      <div className="row field">
-        <p id="field-text">
-          { displayResult() }
-        </p>
-      </div>
-      <div className="row">
-        <div className="button gray" onClick={() => { clickButton(calculatorObj, 'AC'); }}>AC</div>
-        <div className="button gray" onClick={() => { clickButton(calculatorObj, '+/-'); }}>+/-</div>
-        <div className="button gray" onClick={() => { clickButton(calculatorObj, '%'); }}>%</div>
-        <div className="button orange" onClick={() => { clickButton(calculatorObj, '÷'); }}>÷</div>
-      </div>
-      <div className="row">
-        <div className="button gray" onClick={() => { clickButton(calculatorObj, '7'); }}>7</div>
-        <div className="button gray" onClick={() => { clickButton(calculatorObj, '8'); }}>8</div>
-        <div className="button gray" onClick={() => { clickButton(calculatorObj, '9'); }}>9</div>
-        <div className="button orange" onClick={() => { clickButton(calculatorObj, 'x'); }}>X</div>
-      </div>
-      <div className="row">
-        <div className="button gray" onClick={() => { clickButton(calculatorObj, '4'); }}>4</div>
-        <div className="button gray" onClick={() => { clickButton(calculatorObj, '5'); }}>5</div>
-        <div className="button gray" onClick={() => { clickButton(calculatorObj, '6'); }}>6</div>
-        <div className="button orange" onClick={() => { clickButton(calculatorObj, '-'); }}>-</div>
-      </div>
-      <div className="row">
-        <div className="button gray" onClick={() => { clickButton(calculatorObj, '1'); }}>1</div>
-        <div className="button gray" onClick={() => { clickButton(calculatorObj, '2'); }}>2</div>
-        <div className="button gray" onClick={() => { clickButton(calculatorObj, '3'); }}>3</div>
-        <div className="button orange" onClick={() => { clickButton(calculatorObj, '+'); }}>+</div>
-      </div>
-      <div className="row">
-        <div className="big-button gray" onClick={() => { clickButton(calculatorObj, '0'); }}>0</div>
-        <div className="button gray" onClick={() => { clickButton(calculatorObj, '.'); }}>.</div>
-        <div className="button orange" onClick={() => { clickButton(calculatorObj, '='); }}>=</div>
-      </div>
-    </div>
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/calc">
+          <div className="calc-container">
+            <div className="calc-text homeContainer">
+              <h2>Let&apos;s do some math!</h2>
+            </div>
+            <div className="table">
+              <div className="row field">
+                <p id="field-text">
+                  { displayResult() }
+                </p>
+              </div>
+              <div className="row">
+                <div className="button gray" onClick={() => { clickButton(calculatorObj, 'AC'); }}>AC</div>
+                <div className="button gray" onClick={() => { clickButton(calculatorObj, '+/-'); }}>+/-</div>
+                <div className="button gray" onClick={() => { clickButton(calculatorObj, '%'); }}>%</div>
+                <div className="button orange" onClick={() => { clickButton(calculatorObj, '÷'); }}>÷</div>
+              </div>
+              <div className="row">
+                <div className="button gray" onClick={() => { clickButton(calculatorObj, '7'); }}>7</div>
+                <div className="button gray" onClick={() => { clickButton(calculatorObj, '8'); }}>8</div>
+                <div className="button gray" onClick={() => { clickButton(calculatorObj, '9'); }}>9</div>
+                <div className="button orange" onClick={() => { clickButton(calculatorObj, 'x'); }}>X</div>
+              </div>
+              <div className="row">
+                <div className="button gray" onClick={() => { clickButton(calculatorObj, '4'); }}>4</div>
+                <div className="button gray" onClick={() => { clickButton(calculatorObj, '5'); }}>5</div>
+                <div className="button gray" onClick={() => { clickButton(calculatorObj, '6'); }}>6</div>
+                <div className="button orange" onClick={() => { clickButton(calculatorObj, '-'); }}>-</div>
+              </div>
+              <div className="row">
+                <div className="button gray" onClick={() => { clickButton(calculatorObj, '1'); }}>1</div>
+                <div className="button gray" onClick={() => { clickButton(calculatorObj, '2'); }}>2</div>
+                <div className="button gray" onClick={() => { clickButton(calculatorObj, '3'); }}>3</div>
+                <div className="button orange" onClick={() => { clickButton(calculatorObj, '+'); }}>+</div>
+              </div>
+              <div className="row">
+                <div className="big-button gray" onClick={() => { clickButton(calculatorObj, '0'); }}>0</div>
+                <div className="button gray" onClick={() => { clickButton(calculatorObj, '.'); }}>.</div>
+                <div className="button orange" onClick={() => { clickButton(calculatorObj, '='); }}>=</div>
+              </div>
+            </div>
+          </div>
+        </Route>
+        <Route path="/quote">
+          <Quote />
+        </Route>
+        <Route path="/*">
+          <NotMatch />
+        </Route>
+      </Switch>
+    </>
   );
 };
 Calculator.propTypes = {
